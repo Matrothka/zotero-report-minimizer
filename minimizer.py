427c26965b440df96b73b03c04c2fc0e0524d23b
Matthew Roth <14863694+Matrothka@users.noreply.github.com>
2019-07-28 16:26:47 -0400

    Implement removal of hardcoded fields
    Script can remove 4 fields from the file supplied through CLI argument. The result is written to assets\out.html

--- minimizer.py
+++ minimizer.py
@@ -1,13 +1,28 @@
 from bs4 import BeautifulSoup
+from bs4.element import NavigableString
 import sys
 
 
 def main():
-    print("Trimming file: ", sys.argv[0])
+    # hard-coded fields below will be removed from the output
+    filter_fields = ('Type', 'Report Type', 'Date Added', 'Modified')
+
+    # open the file supplied as a paramter and create bs4 object
     with open(sys.argv[1]) as f:
         soup = BeautifulSoup(f, 'html.parser')
-    # print(soup.prettify())
 
+    # iterate through through articles and remove fields
+    for article in soup.body.ul.children:
+        if not isinstance(article, NavigableString):
+            print(article.h2)
+            for field in article.table.tbody.children:
+                if not isinstance(field, NavigableString) and field.th.get_text() in filter_fields:
+                    field.decompose()
+
+    # clean-up formatting and write results to file
+    print(soup.prettify())
+    f = open("assets\out.html", "w+")
+    f.write(soup.prettify())
 
 if __name__ == "__main__":
     main()
