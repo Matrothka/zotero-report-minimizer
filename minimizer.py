7b10c652016d3f4a0d280d8ee126352b4918250a
Matthew Roth <14863694+Matrothka@users.noreply.github.com>
2019-07-30 15:14:12 -0400

    Add ability to remove selected fields
    A hard coded list of fields can be removed from table beneath the title of each article.

--- minimizer.py
+++ minimizer.py
@@ -2,27 +2,32 @@ from bs4 import BeautifulSoup
 from bs4.element import NavigableString
 import sys
 
+# hard-coded fields below will be removed from the output
+filter_fields = ('Type', 'Report Type', 'Date Added',
+                 'Modified', 'Institution')
+
 
 def main():
-    # hard-coded fields below will be removed from the output
-    filter_fields = ('Type', 'Report Type', 'Date Added', 'Modified')
-
     # open the file supplied as a paramter and create bs4 object
     with open(sys.argv[1]) as f:
         soup = BeautifulSoup(f, 'html.parser')
 
     # iterate through through articles and remove fields
-    for article in soup.body.ul.children:
-        if not isinstance(article, NavigableString):
-            print(article.h2)
-            for field in article.table.tbody.children:
-                if not isinstance(field, NavigableString) and field.th.get_text() in filter_fields:
-                    field.decompose()
+    for article in filter(stringFilter, soup.body.ul.children):
+        for field in filter(decomposeFilter, article.table.tbody.children):
+            field.decompose()
 
     # clean-up formatting and write results to file
-    print(soup.prettify())
     f = open("assets\out.html", "w+")
     f.write(soup.prettify())
+
+
+def stringFilter(s):
+    return not isinstance(s, NavigableString)
+
+
+def decomposeFilter(s):
+    return s.th.get_text() in filter_fields and stringFilter(s)
 
 if __name__ == "__main__":
     main()
