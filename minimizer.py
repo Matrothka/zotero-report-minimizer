dba972dd35f3ac39ff3baecf3cb9a8b91c29a6fd
Matthew Roth <14863694+Matrothka@users.noreply.github.com>
2019-07-31 12:20:11 -0400

    Use argparse for argument parsing fixes #2

--- minimizer.py
+++ minimizer.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from bs4.element import NavigableString
 import sys
+import argparse
 
 # hard-coded fields below will be removed from the output
 filter_fields = ('Type', 'Report Type', 'Date Added',
@@ -8,13 +9,33 @@ filter_fields = ('Type', 'Report Type', 'Date Added',
 
 
 def main():
+    global filter_fields
+    # create an argprase object to recieve CLI arguments
+    parser = argparse.ArgumentParser(description="Remove unwanted fields"
+                                     "from a zotero HTML report")
+    parser.add_argument('-s', '--source', type=str, metavar='', required=True,
+                        help='The report to minimize')
+    parser.add_argument('-r', '--remove', metavar='',
+                        default=('Type', 'Report Type',
+                                 'Date Added', 'Modified', 'Institution'),
+                        help='The fields to remove')
+
+    # parse the arguments and store the list of unwanted fields
+    args = parser.parse_args()
+    filter_fields = args.remove.split(" ")
+
+    # replace underscores with spaces and capitalize each word
+    for i, f in enumerate(filter_fields):
+        filter_fields[i] = f.replace('_', ' ').title()
+
     # open the file supplied as a paramter and create bs4 object
-    with open(sys.argv[1]) as f:
+    with open((args.source).strip()) as f:
         soup = BeautifulSoup(f, 'html.parser')
 
-    # iterate through through articles and remove fields
+    # iterate through through articles and remove unwanted fields
     for article in filter(stringFilter, soup.body.ul.children):
         for field in filter(decomposeFilter, article.table.tbody.children):
+            print('decomposing: ' + field.get_text())
             field.decompose()
 
     # clean-up formatting and write results to file
@@ -22,12 +43,17 @@ def main():
     f.write(soup.prettify())
 
 
-def stringFilter(s):
+def stringFilter(s) -> bool:
     return not isinstance(s, NavigableString)
 
 
-def decomposeFilter(s):
-    return s.th.get_text() in filter_fields and stringFilter(s)
+def decomposeFilter(s) -> bool:
+    global filter_fields
+    if stringFilter(s):
+        return s.th.get_text() in filter_fields
+    else:
+        return False
 
+# run main() if invoked directly
 if __name__ == "__main__":
     main()
